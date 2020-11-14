4f7080960ce35307680b0dcb6f64fb97fcf184d9
Alex Sandro Schmidt <alex.schmidt@AVMB.COM.BR>
2020-11-14 14:07:37 -0300

    Adicionado componente para mensagens

--- frontend/src/components/FrmLogin/FrmLogin.jsx
+++ frontend/src/components/FrmLogin/FrmLogin.jsx
@@ -1,33 +1,55 @@
-import { formatMs } from '@material-ui/core'
 import React, { useState } from 'react'
+
 import { useHistory } from 'react-router-dom'
 import {
-    Form, FormGroup, Label, Row, Col, Input, Button
+    Form, FormGroup, Row, Col, Input, Button
 } from 'reactstrap'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login(props){
     let history = useHistory();
+    const [show, setShow] = useState(false);
     const [usuario, setUsuario] = useState('')
     const [senha, setSenha] = useState('')
+    
 
     const [pass] = useState('bobariga')
     const [user] = useState('admin')
-
+    
+    function notificacao(message, type){        
+        if(message.trim !== ''){
+            switch (type) {
+                case 'success':
+                    toast.success(message);
+                    break;
+                case 'warn':
+                    toast.warn(message);
+                    break;
+                case 'error':
+                    toast.error(message);
+                    break;
+                default:
+                    toast.info(message);
+            }
+        }
+    }    
+    
     function handleLogin() {
         
         if(usuario === user && senha === pass){
             localStorage.setItem('@SuaAplicacao:JWT_TOKEN', 'seutokenjwt');            
             history.push('/correcoes')
-        }else{
-            alert('errado')
+        }else{            
+            notificacao('Usuário inválido', 'warn') 
         }
     }
 
     return (
-        <>  
+        <>   <ToastContainer />       
            <Row>
                <Col>
-                    <Form onSubmit={handleLogin}>
+                    <Form >
                         <FormGroup>
                             <Input name="userLogin" value={usuario}
                                    onChange={e => setUsuario(e.target.value)} 
@@ -36,11 +58,13 @@ export default function Login(props){
                             <Input type="password" name="senhaLogin" id="senhaLogin" placeholder="Senha" 
                                    value={senha} onChange={e => setSenha(e.target.value)}
                             />
-                            <Button style={{marginTop:'10px'}} type="submit">Entrar</Button>
+                            <Button style={{marginTop:'10px'}} onClick={e => handleLogin()}>Entrar</Button>
                         </FormGroup>
                     </Form>
                </Col>
-           </Row>
+              
+           </Row>     
+              
         </>
     )
 }
